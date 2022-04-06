911d06ecbb125063946d98bb82a389d5d4a9b7a8
Maarinafgg <marinafrancisca@yahoo.com.br>
2022-04-07 08:53:59 +1000

    Adding id for Humidity and wind speed

--- javas.js
+++ javas.js
@@ -3,13 +3,17 @@ console.log(response.data);
 let temperature = document.querySelector("#temperature");
 let city = document.querySelector("#city");
 let description = document.querySelector("#weatherDiscription");
-let feelslike = document.querySelector("#feelsLike")
+let feelslike = document.querySelector("#feelsLike");
+let humidity = document.querySelector("#humidity");
+let wind = document.querySelector("#wind");
+
 temperature.innerHTML = Math.round (response.data.main.temp);
 city.innerHTML = response.data.name;
 description.innerHTML = response.data.weather[0].main;
 feelslike.innerHTML = Math.round (response.data.main.feels_like);
+humidity.innerHTML = response.data.main.humidity;
+wind.innerHTML = Math.round (response.data.wind.speed);
 }
-
 
 
 let apiKey = "9078bdda44af2b0743ddeae89e1d419b";
