1d4672989e73f9da8b7fcf70244d1c74c9d4c44f
Maarinafgg <marinafrancisca@yahoo.com.br>
2022-04-15 13:28:27 +1000

    creating a function to get lat and lon coordinates

--- javas.js
+++ javas.js
@@ -19,6 +19,13 @@ let weekDays = ["Sunday",
   }
   date.innerHTML = `${day} ${hours}:${minutes}`;
 
+  function getForcast(coordinates){
+      console.log(coordinates);
+      let apiKey = "9078bdda44af2b0743ddeae89e1d419b";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+    }
+
 function displayTemperature (response){
 let temperature = document.querySelector("#temperature");
 let cityElem = document.querySelector("#city");
@@ -37,6 +44,9 @@ feelslike.innerHTML = Math.round (response.data.main.feels_like);
 humidity.innerHTML = response.data.main.humidity;
 wind.innerHTML = Math.round (response.data.wind.speed);
 iconElem.setAttribute ("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+
+getForcast(response.data.coord)
+
 }
 
 
